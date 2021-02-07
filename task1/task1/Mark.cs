e543c6d674b45a5f2c7eb28380da88cedd11de18
CrazyTapor <70747386+CrazyTapor@users.noreply.github.com>
2021-02-07 20:01:29 +0300

    initial commit

---
+++ task1/task1/Mark.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_2
+{
+    class Mark
+    {
+        public string subject { get; set; }
+
+        public int mark { get; set; }
+
+        public Mark()
+        {
+            subject = subject;
+            mark = mark;
+        }
+        public void Passport()
+        {
+            Console.WriteLine($" subject: {subject} - {mark}");
+        }
+
+    }
+
+}
