471b1d50eff267f95fd2f0a2740c3b052553bf46
CrazyTapor <70747386+CrazyTapor@users.noreply.github.com>
2021-04-28 15:25:43 +0300

    commit

--- task1/task1/Student.cs
+++ task1/task1/Student.cs
@@ -4,30 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task1_2
+namespace Task1_2
 {
     class Student
     {
-        private string first_name { get; set; }
-        private string last_name { get; set; }
-        private int age { get; set; }
-        private string group { get; set; }
-        private string university { get; set; }
-        public Student(string n, string m, int a, string p, string k) { first_name = n; last_name = m; age = a; university = p; group = k; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string group { get; set; }
+        public string university { get; set; }
         public int n = Convert.ToInt32(Console.ReadLine());  //ввод количества оценнок
 
         Mark[] marks = new Mark[20];
         // персоналные данные о студенте
-        public void ownPerson()
+        public void OwnPerson(string name, out string firstNames, out string lastName, ref int age, ref string university, ref string group)
         {
+            int i = name.LastIndexOf(' ');
+            firstNames = name.Substring(0, i);
+            lastName = name.Substring(i + 1);
+            age = 20; university = "Gstu of sohoi"; group = "PM-31";
             Console.WriteLine("STUDENT");
-            Console.WriteLine($" Name:{first_name} {last_name} Age:{age} \n University: {university}\n Group:{group}");
+            Console.WriteLine($" Name: {firstNames} {lastName} Age:{age} \n University: {university}\n Group:{group}");
             Console.WriteLine("\n");
         }
-
-
         //ввод оценок        
-        public void Mark()
+        public void InputMark()
         {
             Console.WriteLine("enter ratings:");
             for (int i = 0; i < n; i++)
@@ -47,7 +48,7 @@ namespace task1_2
             Console.Clear();
         }
         //ввывод оценнок и предметов
-        public void Mark2()
+        public void OutputMark()
         {
             Console.WriteLine("RATING LIST");
             for (int i = 0; i < n; i++)
@@ -57,18 +58,14 @@ namespace task1_2
             Console.WriteLine("\n");
         }
         //средний балл
-        public void GetAvgMark()
+        public int GetAvgMark(params int[] Mark)
         {
-
-            int sum = 0; double average = 0;
+            int sum = 0;
             for (int i = 0; i < n; i++)
-            {
                 sum += marks[i].mark;
-            }
-            average = sum / n;
-            Console.WriteLine($"Average:{average}");
-            Console.WriteLine("\n");
+            return sum / n;
         }
+
         //обнуление всех оценнок
         public void ResetAllMarks()
         {
@@ -89,8 +86,7 @@ namespace task1_2
             {
                 marks[i].Passport();
             }
-            Console.WriteLine("\n");
+        Console.WriteLine("\n");
         }
-
     }
 }
